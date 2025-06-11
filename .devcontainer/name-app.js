7883052174da74e28e70f470d71726a84d2c95a4
MT 福冨 楓 <kaede.fukutomi@metateam.co.jp>
2025-06-11 17:29:15 +0900

    名前表示アプリ修正済み2

--- .devcontainer/name-app.js
+++ .devcontainer/name-app.js
@@ -1,16 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("confirmedBtn");
-  const input = document.getElementById("yourName");
-  const messageElement = document.getElementById("Confirm");
+const button = document.getElementById("ConfirmedBtn");
+const input = document.getElementById("YourName");
+const messageElement = document.getElementById("Confirm");
 
-  button.addEventListener("click", () => {
-    const name = input.value.trim();
-
-    if (name) {
-      messageElement.textContent = `こんにちは ${name} さん！`;
-    } else {
-      messageElement.textContent = "名前を入力してください。";
-    }
-    messageElement.classList.remove("hidden");
-  });
+button.addEventListener("click", () => {
+  const name = input.value.trim();
+  if (name) {
+    messageElement.textContent = `こんにちは ${name} さん！`;
+  } else {
+    messageElement.textContent = "名前を入力してください。";
+  }
+  messageElement.classList.remove("hidden");
 });
