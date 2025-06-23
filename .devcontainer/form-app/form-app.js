04ce7a11b2fada932dad258590717e64c95c684a
MT 福冨 楓 <kaede.fukutomi@metateam.co.jp>
2025-06-23 12:47:49 +0900

    アンケートフォームレビュー反映済み

--- .devcontainer/form-app/form-app.js
+++ .devcontainer/form-app/form-app.js
@@ -1,5 +1,5 @@
-const form = document.getElementById("survey-form");
-const username = document.getElementById("user-name");
+const form = document.getElementById("surveyForm");
+const userName = document.getElementById("userName");
 const age = document.getElementById("age");
 const job = document.getElementById("job");
 const introduction = document.getElementById("introduction");
@@ -7,41 +7,32 @@ const introduction = document.getElementById("introduction");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  document.getElementById("error-user-name").style.display = "none";
-  document.getElementById("error-age").style.display = "none";
-  document.getElementById("error-job").style.display = "none";
+  document.getElementById("errorUserName").style.display = "none";
+  document.getElementById("errorAge").style.display = "none";
+  document.getElementById("errorJob").style.display = "none";
 
   let hasError = false;
 
-  if (username.value.trim() === "") {
-    document.getElementById("error-user-name").style.display = "block";
+  if (userName.value.trim() === "") {
+    document.getElementById("errorUserName").style.display = "block";
     hasError = true;
   }
 
   if (age.value.trim() === "" || age.value >= 18) {
-    document.getElementById("error-age").style.display = "block";
+    document.getElementById("errorAge").style.display = "block";
     hasError = true;
   }
 
   if (job.value.trim() === "") {
-    document.getElementById("error-job").style.display = "block";
+    document.getElementById("errorJob").style.display = "block";
     hasError = true;
   }
 
   const formData = {
-    username: username.value.trim(),
+    userName: userName.value.trim(),
     age: age.value.trim(),
     job: job.value,
     introduction: introduction.value.trim(),
   };
-
-  if (import.meta.env.VITE_APP_ENV === "prod") {
-    console.log =
-      console.info =
-      console.debug =
-      console.warn =
-      console.error =
-        () => {};
-  }
   console.log("送信データ(JSON):", JSON.stringify(formData));
 });
